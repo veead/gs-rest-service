67f3551f2f46c979bcd65755ed174ecb0311ec35
Vee <narayanan.venkiteswaran@appdirect.com>
2018-07-25 18:04:17 -0700

    Strict xss check for json payload

---
+++ initial/src/main/java/hello/GreetingController.java
@@ -0,0 +1,26 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.*;
+import com.google.json.JsonSanitizer;
+import javax.validation.*;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+
+    @PostMapping("/greeting")
+    public Greeting createGreeting(@RequestBody @Valid Greeting greeting) {
+	//String content = JsonSanitizer.sanitize( greeting.getContent() );
+	String content = greeting.getContent();
+        return new Greeting(counter.incrementAndGet(), content);
+    }
+}
